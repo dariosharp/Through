e9f9b2f281e93555692e4ec7164f540ce5d802d1
dariosharp <dario.mail1@gmail.com>
2023-07-14 18:24:40 +0200

    added logging

--- extractor.py
+++ extractor.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python                                                                                     
 
 
-import argparse
+import argparse, logging
 
+logger = logging.getLogger('Logger')                
+logger.setLevel(logging.INFO)
 
 def main(binary, function, libraries):
-    print(binary, function, libraries)
+    logger.debug("Binary: {} Function: {} Library path: {}".format(binary, function, libraries))
 
 if __name__ == "__main__":
+    handler = logging.StreamHandler()
+    formatting = logging.Formatter('[%(levelname)s] %(message)s')
+    handler.setFormatter(formatting)
     parser = argparse.ArgumentParser(description='Through, sourth among libraries')
     parser.add_argument('-b', '--binary', type=str, help='Binary to analyze', required=True)
     parser.add_argument('-f', '--function', type=str, help='Function you are looking for', required=True)
     parser.add_argument('-l', '--libraries', type=str, help='Libraries path', required=True)
+    parser.add_argument('-v','--verbosity', action='store_true', help='Debugging verbosity')
     args = parser.parse_args()
+    if args.verbosity:
+        logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
     main(args.binary,args.function, args.libraries)
 
