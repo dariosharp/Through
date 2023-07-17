20791e8751f258a5e4a215ad7398520f55849865
dariosharp <dario.mail1@gmail.com>
2023-07-17 16:14:34 +0200

    including ida compatibility

--- main.py
+++ main.py
@@ -14,26 +14,30 @@ def main(pathbinary, function, pathlibraries, ida):
     through = Through(pathbinary,function,pathlibraries,ida)
     for f,l in through.getFunctionPerLib():
         logger.info("Found \"{}\" in \"{}\"".format(f, l))
-    
+
+def windowIDA():
+
+    return os.path.basename((idaapi.get_input_file_path())),"system","Libs"
+
+
 if __name__ == "__main__":
     handler = logging.StreamHandler()
     formatting = logging.Formatter('[%(levelname)s] %(message)s')
     handler.setFormatter(formatting)
     if platform.system() == "Windows":
-        import os
-        binary = os.path.basename((idaapi.get_input_file_path()))
-        function = "system"
-        libraries = "..\\libs"
+        logger.addHandler(handler)
+        binary, function, libraries = windowIDA()
         main(binary, function, libraries)
-    parser = argparse.ArgumentParser(description='Through, finding vulnearbilities through libraries')
-    parser.add_argument('-b', '--binary', type=str, help='Binary to analyze', required=True)
-    parser.add_argument('-f', '--function', type=str, help='Function you are looking for', required=True)
-    parser.add_argument('-l', '--libraries', type=str, help='Libraries path', required=True)
-    parser.add_argument('-v','--verbosity', action='store_true', help='Debugging verbosity')
-    args = parser.parse_args()
-    if args.verbosity:
-        logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
-    main(args.binary,args.function, args.libraries)
+    if platform.system() == "Linux":
+        parser = argparse.ArgumentParser(description='Through, finding vulnearbilities through libraries')
+        parser.add_argument('-b', '--binary', type=str, help='Binary to analyze', required=True)
+        parser.add_argument('-f', '--function', type=str, help='Function you are looking for', required=True)
+        parser.add_argument('-l', '--libraries', type=str, help='Libraries path', required=True)
+        parser.add_argument('-v','--verbosity', action='store_true', help='Debugging verbosity')
+        args = parser.parse_args()
+        if args.verbosity:
+            logger.setLevel(logging.DEBUG)
+        logger.addHandler(handler)
+        main(args.binary,args.function, args.libraries)
 
 
