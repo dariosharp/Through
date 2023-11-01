1d2d019e05d5411fd41e755c3796e8a8e74ce400
dariosharp <dario.mail1@gmail.com>
2023-11-01 16:10:45 +0100

    set tbale not editable and more spece for the header labels

--- through/libs/results.py
+++ through/libs/results.py
@@ -1,5 +1,5 @@
 from idaapi import PluginForm
-from PyQt5 import QtWidgets
+from PyQt5 import QtCore,QtWidgets
 
 class ResultClass(PluginForm):
     def OnCreate(self, form):
@@ -14,14 +14,17 @@ class ResultClass(PluginForm):
         self.table.setRowCount(0)
         self.table.setColumnCount(3)
         self.table.setHorizontalHeaderLabels(["Library Name", "Function Imported Affected", "Exploit address"])
+        header = self.table.horizontalHeader()
+        header.setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents) 
         layout.addWidget(self.table)
-
         self.parent.setLayout(layout)
 
     def add_row(self, data):
         rowPosition = self.table.rowCount()
         self.table.insertRow(rowPosition)
         for col, item in enumerate(data):
+            item = QtWidgets.QTableWidgetItem(item)
+            item.setFlags(QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable)
             self.table.setItem(rowPosition, col, QtWidgets.QTableWidgetItem(item))
 
     def OnClose(self, form):
